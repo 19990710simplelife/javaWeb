28b74db90cc01e40dcbb48a7c49e3e6372985758
19990710simplelife <1753848616@qq.com>
2022-09-16 11:44:13 +0800

    stream流的创建及常用api

--- day04/src/cn/simplelife/work/_03streamapi/StreamApiSelect.java
+++ day04/src/cn/simplelife/work/_03streamapi/StreamApiSelect.java
@@ -12,7 +12,7 @@ import java.util.function.Predicate;
  * @Version 1.0
  */
 
-public class StreamApi {
+public class StreamApiSelect {
     public static void main(String[] args) {
         // 需求1：对制定的集合进行过滤出偶数并去重复并保留其中三个并跳过指定1个后进行打印输出
         List<Integer> list = Arrays.asList(1, 8, 5, 6, 21, 45, 265, 15, 12, 33, 32, 36, 6);
