6790b641fa23b199952f012845084e6d3846d24a
19990710simplelife <1753848616@qq.com>
2022-10-25 11:38:43 +0800

    作用域操作

---
+++ day15/src/cn/simplelife/work/_03scope/SecondServlet.java
@@ -0,0 +1,12 @@
+package cn.simplelife.work._03scope;
+
+/**
+ * @ClassName SecodeServlet
+ * @Description
+ * @Author simplelife
+ * @Date 2022/10/25 11:37
+ * @Version 1.0
+ */
+
+public class SecondServlet {
+}
